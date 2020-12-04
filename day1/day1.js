0afedc3cd324159991d7c9b15617b8b7cedf715a
Cecilia Lobo <cecilialobo92@gmail.com>
2020-12-04 17:07:47 +0100

    Refactoring

--- day1/day1.js
+++ day1/day1.js
@@ -2,32 +2,33 @@ const fs = require("fs");
 
 const logicToFindTheSumOfTwo = () => {
     const inputData = fs.readFileSync("inputday1.txt", "utf8");
-    const dataArray = inputData.split('\n');    
+    const dataArray = inputData.split("\n");    
     dataArray.forEach((item) => {
         dataArray.forEach((item2) => {
             if (Number(item) + Number(item2) === 2020) {
                 return Number(item) * Number(item2);
             }
-            return 0;
+            return;
         })
     })
 }
 
 const logicToFindTheSumOfThree = () => {
     const inputData = fs.readFileSync("inputday1.txt", "utf8");
-    const dataArray = inputData.split('\n');    
+    const dataArray = inputData.split("\n");    
     dataArray.forEach((item) => {
         dataArray.forEach((item2) => {
             dataArray.forEach((item3) => {
                 if (Number(item) + Number(item2) + Number(item3) === 2020) {
-                    const result = Number(item) * Number(item2) * Number(item3);
-                    console.log(result);
+                    return Number(item) * Number(item2) * Number(item3);
                 }
             })
-            return 0;
+            return;
         })
     })
 }
 
-logicToFindTheSumOfThree();
+//Can improve this with binary search
+//.readFileSync
+//.split("\n")
 
